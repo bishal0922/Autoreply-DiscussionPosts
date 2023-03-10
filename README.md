0a0ec3b0826c8aeeefe488052b5d742018f01db7
Bishal Giri <bishal0922@gmail.com>
2023-03-10 03:03:14 +0000

    implemented api stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Autoreply-DiscussionPosts
 Tired of generating the mandatory discussion post replies, look no further. I never plan to use this in my classes of course, only for fun!
+
+
+## SMH
+> {'error': {'message': 'You exceeded your current quota, please check your plan and billing details.', 'type': 'insufficient_quota', 'param': None, 'code': None}}
--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
 
 import os
+import json
 import openai
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-print("Hello World!")
 
 openai.organization = "org-SOiwnRT1gkRHkbx9s7KazzKl"
 api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = api_key
 
-print(openai.Model.list())
+ENDPOINT = "https://api.openai.com/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
+
+data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [{
+        "role": "user",
+        "content": "Hello!"
+    }]
+}
+
+response = requests.post(ENDPOINT, data=json.dumps(data), headers=headers, timeout=10)
+
+# Get completion text from response
+completion_text = response.json()
+
+# Print completion text
+print(completion_text)
